ff23d82d7f738dea3373fbfafa8ce6f3f4d829e8
cjkook <cjko.audio@gmail.com>
2020-07-20 14:07:09 -0600

    text formatting, badges

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# readme-generator
-A node.js application for automating the readme creation process.
+# asdf
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0) 
+
+  * [Description](#description)
+  * [Install](#install)
+  * [Usage](#usage)
+  * [Contributions](#contributions)
+  * [License](#license)
+  * [Contact](#contact)
+## Description
+asdf
+## Install
+asdf
+
+## Usage
+adsf
+
+## Contributions
+adsf
+
+## License
+GNU
+
+## Contact
+[asdf](https://github.com/asdf)
+asdf
+
+![Screenshot](asdf)
+
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const axios = require("axios");
-const readme = {
+const util = require("util");
+// ! promisify in act 34
+let readme = {
   title: "",
   description: "",
   install: "",
@@ -15,35 +17,41 @@ const readme = {
   image: "",
 };
 
+const licenseBadge = [
+  "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)",
+  "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+  "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+];
+
 // GIVEN a command-line application that accepts user input
 // WHEN I am prompted for information about my application repository
 // THEN a quality, professional README.md is generated with the title of your project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 // WHEN I enter my project title
 // THEN this is displayed as the title of the README
-// ! create readme.md
+// create readme.md
 // ? create json to save for later?
-// ! title input
+//  title input
 
 // WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// ! description input
-// ! installation input
-// ! usage input
-// ! contribution input
-// ! test input
+//  description input
+//  installation input
+//  usage input
+//  contribution input
+//  test input
 
 // THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
 // ! create sections
 // ! append to file
 
 // WHEN I choose a license for my application from a list of options
-// ! list options
+// list options
 // THEN a badge for that license is added hear the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// ! github badge?
 
 // WHEN I enter my GitHub username
-// ! github username input
+//  github username input
 // THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
 // ! find github account?
+// ! async await promises
 // ! get url to profile
 // ! append to section
 
@@ -60,7 +68,7 @@ const readme = {
 inquirer
   .prompt([
     {
-      type: "checkbox",
+      type: "list",
       message: "Create a new file or edit an existing one?",
       name: "action",
       choices: ["new", "existing"],
@@ -74,15 +82,6 @@ inquirer
 function fnNew() {
   inquirer
     .prompt([
-      //   title: "",
-      // description: "",
-      // install: "",
-      // usage: "",
-      // test: "",
-      // license: "",
-      // badge: "",
-      // gitUser: "",
-      // email: ""
       {
         type: "input",
         message: "Please enter a title for your project",
@@ -92,6 +91,11 @@ function fnNew() {
         type: "input",
         message: "Please enter a description for your project",
         name: "description",
+      },
+      {
+        type: "input",
+        message: "Please enter the installation for your project",
+        name: "install",
       },
       {
         type: "input",
@@ -109,15 +113,10 @@ function fnNew() {
         name: "test",
       },
       {
-        type: "checkbox",
+        type: "list",
         message: "Please enter a license for your project",
         name: "license",
-        choices: [],
-      },
-      {
-        type: "input",
-        message: "Please enter a badge for your project",
-        name: "badge",
+        choices: ["Creative Commons", "MIT", "GNU"],
       },
       {
         type: "input",
@@ -136,24 +135,11 @@ function fnNew() {
       },
     ])
     .then(function (res) {
-      // console.log(res);
-      for (let para in readme) {
-        para = res[`${para}`];
-      }
-      readme.title = res.title;
-      readme.description = res.description;
-      readme.usage = res.usage;
-      readme.contribute = res.contribute
-      readme.test = res.test;
-      readme.license = res.license;
-      readme.badge = res.badge;
-      readme.gitUser = res.gitUser;
-      readme.email = res.email;
-      readme.image = res.image;
+      readme = res;
 
       console.log(readme);
 
-      fnWrite()
+      fnWrite();
     });
 }
 
@@ -162,17 +148,66 @@ function fnOld() {
 }
 
 function fnWrite() {
-  // fs.writeFile("README.md", rawText, function(err)
-  fs.writeFile() {
-    if(err) throw err;
-    // write text file
-  
+  let rawText = "";
+
+  // title
+  rawText += `# ${readme.title}\n\r`;
+
+  // badge
+
+  switch (readme.license) {
+    case "Creative Commons":
+      rawText += `${licenseBadge[0]} `;
+      break;
+    case "MIT":
+      rawText += `${licenseBadge[1]} `;
+      break;
+    case "GNU":
+      rawText += `${licenseBadge[2]} `;
+      break;
   }
-  
+
+  // Table of contents
+  rawText += `\n\r
+  * [Description](#description)
+  * [Install](#install)
+  * [Usage](#usage)
+  * [Contributions](#contributions)
+  * [License](#license)
+  * [Contact](#contact)\n\r`;
+
+  //description
+  rawText += `## Description\n\r${readme.description}\n\r`;
+
+  // install
+  rawText += `## Install\n\r${readme.install}\n\r\n\r`;
+
+  // usage
+  rawText += `## Usage\n\r${readme.usage}\n\r\n\r`;
+
+  // contributions
+  rawText += `## Contributions\n\r${readme.contribute}\n\r\n\r`;
+
+  // test
+
+  // license
+  rawText += `## License\n\r${readme.license}\n\r\n\r`;
+
+  // gituser // email
+  rawText += `## Contact\n\r[${readme.gitUser}](https://github.com/${readme.gitUser})\n\r${readme.email}\n\r\n\r`;
+
+  // image
+  rawText += `![Screenshot](${readme.image})\n\r`;
+
+  // fs.writeFile("README.md", rawText, function(err)
+  fs.writeFile("README.md", rawText, function (err) {
+    if (err) throw err;
+
+    console.log("README.md generated.");
+  });
 }
 
-
-//   console.log("Success!");
+//
 
 // });
 
